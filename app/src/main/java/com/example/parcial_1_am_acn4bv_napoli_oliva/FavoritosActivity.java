e9378bbf236085f50c6ad1df495e88d032278a60
gusoliva85 <gusoliva85@gmail.com>
2025-12-15 20:39:15 -0300

    Favoritos en FireBase (nueva version)

--- app/src/main/java/com/example/parcial_1_am_acn4bv_napoli_oliva/FavoritosActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4bv_napoli_oliva/FavoritosActivity.java
@@ -1,64 +1,135 @@
 package com.example.parcial_1_am_acn4bv_napoli_oliva;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-public class FavoritosActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private LinearLayout contenedor;
+public class FavoritosActivity extends AppCompatActivity {
+    private LinearLayout listaFavoritos;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+    private String currentUserId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favoritos);
 
-        contenedor = findViewById(R.id.contenedorListaFavoritos);
-        Button btnVolver = findViewById(R.id.btnVolverMain);
+        // Firebase
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        // datos del front
+        listaFavoritos = findViewById(R.id.listaFavoritos);
+        Button btnVolver = findViewById(R.id.btnVolverFavoritos);
 
         btnVolver.setOnClickListener(v -> finish());
 
-        cargarFavoritosDeFirebase();
+        // Chequea y carga datos de favoritos
+        checkAuthenticationAndLoadFavorites();
     }
 
-    private void cargarFavoritosDeFirebase() {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private void checkAuthenticationAndLoadFavorites(){
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null){
+            Toast.makeText(this, "Debe iniciar sesion.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        currentUserId = currentUser.getUid();
+        cargarFavoritos();
+    }
 
+    private void cargarFavoritos(){
+        Toast.makeText(this, "Cargando tus favoritos...", Toast.LENGTH_SHORT).show();
+        listaFavoritos.removeAllViews(); // limpia y carga
+
+        // filtrar el userID
         db.collection("favoritos")
+                .whereEqualTo("userId", currentUserId)
                 .get()
                 .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        contenedor.removeAllViews();
-
-                        // recorre los resultados
-                        for (QueryDocumentSnapshot document : task.getResult()) {
+                    if (task.isSuccessful()){
+                        List<Pelicula> favoritos = new ArrayList<>();
+                        for (QueryDocumentSnapshot document : task.getResult()){
+                            // trae el objeto
                             Pelicula p = document.toObject(Pelicula.class);
-                            agregarVistaPelicula(p);
+                            favoritos.add(p);
                         }
 
-                        if (contenedor.getChildCount() == 0) {
-                            Toast.makeText(this, "Aún no tienes favoritos", Toast.LENGTH_SHORT).show();
+                        // si favoritos no encuentra nada, no muestra que no hay nada, sino muestra los favoritos
+                        if (favoritos.isEmpty()){
+                            mostrarMensajeVacio();
+                        } else {
+                            mostrarPeliculas(favoritos);
                         }
                     } else {
-                        Toast.makeText(this, "Error al cargar datos", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, "Error al cargar favoritos: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
                     }
                 });
     }
 
-    private void agregarVistaPelicula(Pelicula p) {
-        TextView texto = new TextView(this);
-        texto.setText("★ " + p.getTitulo() + " (" + p.getAnio() + ")");
-        texto.setTextSize(18);
-        texto.setTextColor(getResources().getColor(android.R.color.white));
-        texto.setPadding(0, 20, 0, 20);
-
-        contenedor.addView(texto);
+    private void mostrarMensajeVacio() {
+        TextView txtVacio = new TextView(this);
+        txtVacio.setText("No tienes peliculas favoritas aun. Podes agregarlas desde la cartelera!");
+        txtVacio.setTextColor(getResources().getColor(R.color.colorTextoSecundario));
+        txtVacio.setTextSize(18);
+        txtVacio.setPadding(32, 32, 32, 32);
+        listaFavoritos.addView(txtVacio);
     }
+
+    private void mostrarPeliculas (List<Pelicula> lista){
+        listaFavoritos.removeAllViews();
+
+        for (Pelicula p : lista){
+            View tarjetaView = getLayoutInflater().inflate(R.layout.item_pelicula, listaFavoritos, false);
+
+            // info de peliculas
+            ImageView img = tarjetaView.findViewById(R.id.imgPelicula);
+            TextView titulo = tarjetaView.findViewById(R.id.txtTituloPelicula);
+            TextView desc = tarjetaView.findViewById(R.id.txtSubtituloPelicula);
+
+            // Completar la info de las peliculas
+            Glide.with(this)
+                    .load(p.getUrlImagen())
+                    .into(img);
+
+            titulo.setText(p.getTitulo());
+            desc.setText(p.getGenero() + " • " + p.getAnio());
+
+            // datos de los favoritos
+            tarjetaView.setOnClickListener(v -> {
+                Intent intent = new Intent(this, DetallePeliculaActivity.class);
+                intent.putExtra("PELICULA_SELECCIONADA", p);
+                startActivity(intent);
+            });
+
+            listaFavoritos.addView(tarjetaView);
+        }
+    }
+
+    // recargar
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (currentUserId != null) {
+            cargarFavoritos();
+        }
+    }
+
 }
